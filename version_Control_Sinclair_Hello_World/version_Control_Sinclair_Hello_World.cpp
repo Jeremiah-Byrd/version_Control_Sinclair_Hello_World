eeeb4364760f06381983e4d6481e97902b6af99a
byrd62162 <byrd62162@151-W10-20>
2022-03-18 10:33:06 -0400

    Add project files.

---
+++ version_Control_Sinclair_Hello_World/version_Control_Sinclair_Hello_World.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+
+	//Exiting program
+	cout << "\n\nTo exit the program press enter...";
+	cin.get();
+	return 0;
+}
